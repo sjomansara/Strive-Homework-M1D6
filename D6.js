b452e67dee2414ea5c807d955394cd7bc42c072e
sjomansara <sara.sjoman@hotmail.com>
2021-06-28 10:17:45 +0200

    solved Ex.C

--- D6.js
+++ D6.js
@@ -33,6 +33,10 @@ console.log(sum)
     Create a variable called "random" and assign to it a random number between 0 and 20 (it should be randomly created at each execution).
 */
 
+let random = Math.random() * 20
+
+console.log(random)
+
 /* Ex.D
     Create a variable called "me" and assign to it an object containing the following information: name = your name, surname = your surname, age = your age.
 */
