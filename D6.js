d9f222eb48fb6802d7ff75bdcc462ac705e39dbb
sjomansara <sara.sjoman@hotmail.com>
2021-06-28 12:05:51 +0200

    solved Ex.3

--- D6.js
+++ D6.js
@@ -103,6 +103,12 @@ console.log(whoIsBigger(100, 20))
     Ex. splitMe("I love coding") => returns ["I", "Love", "Coding"]
 */
 
+function SplitMe(mySentence) {
+  return mySentence.split(" ")
+}
+
+console.log(SplitMe("I love coding")) 
+
 /* Ex.4
     Write a function called "deleteOne" which receives a string and a boolean as parameters. If the boolean value is true it should return the string without the first letter, otherwise it should remove the last one from it.
 */
